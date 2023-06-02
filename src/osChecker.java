be2965e9283fa740fd62ae5dc1441f77e9be6249
MRyan339 <maher.ry@northeastern.edu>
2023-06-01 20:37:47 -0500

    Forgot to add osChecker class

---
+++ src/osChecker.java
@@ -0,0 +1,19 @@
+public class osChecker {
+    
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    public int whichOS(){
+
+        System.out.println(OS);
+        
+        if(OS.contains("win")){
+            System.out.println(0);
+            return 0;
+        } else {
+            System.out.println(1);
+            return 1;
+        }
+
+    }
+
+}
